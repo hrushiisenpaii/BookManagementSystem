a745a68ad6d8e46cba4a8fa4933144dca629d1d7
Hrushikesh Patra <hrushikesh.patra24@gmail.com>
2022-07-23 23:43:08 +0530

    added the cpp file

---
+++ bookshopmanagement.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<mysql/mysql.h>
+
+using namespace std;
+#define ll              long long
+#define nl              "\n"
+#define rep(i,a,b)      for(int i=a;i<b;i++)
+
+
+void solve()
+{
+  
+}
+
+int main(int argc, char const *argv[])
+{   
+	int t;
+	cin>>t;
+
+	while(t>0)
+	{
+		solve();
+		t--;
+	}
+
+return 0;
+}
